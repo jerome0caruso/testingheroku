8b9cb5ea4131e8e01b5850f525402baed45fac9d
jerome0caruso <jerome0caruso@gmail.com>
2022-03-08 17:18:52 -0600

    added database

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const cool = require('cool-ascii-faces');
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 
 express()
@@ -11,6 +19,18 @@ express()
   .get('/cool', (req, res) => res.send(cool()))
   .get('/', (req, res) => res.render('pages/index'))
   .get('/times', (req, res) => res.send(showTimes()))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results);
+      client.release();
+    } catch (err) {
+      console.log(err);
+      res.send("Error" + err);
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
